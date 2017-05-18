da6933ce59fc38b9fc20840c93f74e6b08b32c13
Sebastian Lopez <sebastian-lopez77@hotmail.com>
2017-05-18 17:51:10 -0500

    18 mayo

--- js/map.js
+++ js/map.js
@@ -1,17 +1,31 @@
 var mapOptions = {
-      zoom: 8,
-      center: new google.maps.LatLng(-0.1895788, -78.487384),
+      zoom: 17,
+      center: new google.maps.LatLng(-0.188492, -78.488854),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true
 };
 
 function init() {
   var mapOptions = {
-      zoom: 8,
-      center: new google.maps.LatLng(-0.1895788, -78.487384),
+      zoom: 17,
+      center: new google.maps.LatLng(-0.188492, -78.488854),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       disableDefaultUI: true
   };
   var myMap = new google.maps.Map(document.getElementById('myMap'), mapOptions);
+
+  var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      myMap: myMap,
+      icon: iconBase + 'parking_lot_maps.png'
+    });
 }
+
+
+
+
+
+
+
 google.maps.event.addDomListener(window, 'load', init);
